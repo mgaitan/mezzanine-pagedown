7766a4d599ac21f09d3dc91af0d61b701c54a44e
Alvin Mites <alvin@mitesdesign.com>
2016-10-03 21:53:15 -0600

    Django 1.10 compatibility

--- mezzanine_pagedown/urls.py
+++ mezzanine_pagedown/urls.py
@@ -1,7 +1,17 @@
-from django.conf.urls import patterns, url
+from django import VERSION
 
 from .views import MarkupPreview
 
-urlpatterns = patterns('',
-    url(r'^preview/$', MarkupPreview.as_view(), name='preview'),
-)
+
+if VERSION[1] > 9:
+    from django.conf.urls import url
+    urlpatterns = (
+        url(r'^preview/$', MarkupPreview.as_view(), name='preview'),
+    )    
+    
+else:
+    from django.conf.urls import patterns, url
+    urlpatterns = patterns('',
+        url(r'^preview/$', MarkupPreview.as_view(), name='preview'),
+    )
+
