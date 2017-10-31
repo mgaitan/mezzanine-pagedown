36a748854d1dd127b37e8ab38e970d5884cd6645
kensakurai <sakurai.kem@gmail.com>
2017-10-31 17:13:53 +0900

    support latest bleach

--- mezzanine_pagedown/filters.py
+++ mezzanine_pagedown/filters.py
@@ -9,15 +9,15 @@ from bleach import clean
 def _clean(html):
     if settings.PAGEDOWN_USE_MEZZANINE_HTML_ESCAPE:
         return escape(html)
-    else:
-        tags = settings.RICHTEXT_ALLOWED_TAGS
-        attrs = settings.RICHTEXT_ALLOWED_ATTRIBUTES
-        styles = settings.RICHTEXT_ALLOWED_STYLES
-        if LooseVersion('2.0') <= LooseVersion(bleach.__version__) and isinstance(attrs, tuple):
-            attrs = list(attrs)
+
+    tags = settings.RICHTEXT_ALLOWED_TAGS
+    attrs = settings.RICHTEXT_ALLOWED_ATTRIBUTES
+    styles = settings.RICHTEXT_ALLOWED_STYLES
+    if LooseVersion('2.0') <= LooseVersion(bleach.__version__) and isinstance(attrs, tuple):
+        attrs = list(attrs)
 
     return clean(html, tags=tags, attributes=attrs, strip=True,
-                 strip_comments=False, styles=styles)
+                strip_comments=False, styles=styles)
 
 
 def codehilite(content):
